0c53d1dcd0f1285f604837530e82da504d2a2945
Jeff Putz <jeff@popw.com>
2014-05-14 19:03:22 -0400

    Fix for partial commit. Oops.

---
+++ PopForums.Data.Azure/CacheHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.ApplicationServer.Caching;
+using PopForums.Configuration;
+
+namespace PopForums.Data.Azure
+{
+	public class CacheHelper : ICacheHelper
+	{
+		public CacheHelper()
+		{
+			_config = new Config();
+			var factory = new DataCacheFactory();
+			_dataCache = factory.GetCache(CacheName);
+		}
+
+		private Config _config;
+		private DataCache _dataCache;
+		public const string CacheName = "PopForums";
+
+		public void SetCacheObject(string key, object value)
+		{
+			_dataCache.Put(key, value, new TimeSpan(0, 0, 0, _config.CacheSeconds));
+		}
+
+		public object GetCacheObject(string key)
+		{
+			return _dataCache.Get(key);
+		}
+
+		public void RemoveCacheObject(string key)
+		{
+			_dataCache.Remove(key);
+		}
+
+		public void SetCacheObject(string key, object value, double seconds)
+		{
+			_dataCache.Put(key, value, new TimeSpan(0, 0, 0, (int)seconds));
+		}
+
+		public T GetCacheObject<T>(string key)
+		{
+			var cacheObject = _dataCache.Get(key);
+			return (T)cacheObject;
+		}
+	}
+}
