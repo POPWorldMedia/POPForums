2e3752f61e79e2377931f0af9d626ed30083e726
Jeff Putz <jeff@popw.com>
2015-02-05 19:05:27 -0500

    QA translations, CSS tweaks, show check answer, hide reply without user, new version.

--- PopForums.Data.SqlSingleWebServer/Properties/AssemblyInfo.cs
+++ PopForums.Data.SqlSingleWebServer/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("PopForums.Data.SqlSingleWebServer")]
 [assembly: AssemblyCompany("POP World Media, LLC")]
 [assembly: AssemblyProduct("PopForums.Data.SqlSingleWebServer")]
-[assembly: AssemblyCopyright("Copyright ©2014, POP World Media, LLC")]
+[assembly: AssemblyCopyright("Copyright ©2015, POP World Media, LLC")]
 [assembly: AssemblyTrademark("POP Forums is a trademark of POP World Media, LLC")]
-[assembly: AssemblyVersion("13.0.0.14051")]
+[assembly: AssemblyVersion("13.0.0.15021")]
