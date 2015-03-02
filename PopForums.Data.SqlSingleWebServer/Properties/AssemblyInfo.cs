cc2e3e6eff757f000709e6dfbd1ac840e13d132d
Jeff Putz <jeff@popw.com>
2015-03-02 16:41:34 -0500

    Move version for compile to single file.

--- PopForums.Data.SqlSingleWebServer/Properties/AssemblyInfo.cs
+++ PopForums.Data.SqlSingleWebServer/Properties/AssemblyInfo.cs
@@ -1,8 +1,4 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("PopForums.Data.SqlSingleWebServer")]
-[assembly: AssemblyCompany("POP World Media, LLC")]
 [assembly: AssemblyProduct("PopForums.Data.SqlSingleWebServer")]
-[assembly: AssemblyCopyright("Copyright ©2015, POP World Media, LLC")]
-[assembly: AssemblyTrademark("POP Forums is a trademark of POP World Media, LLC")]
-[assembly: AssemblyVersion("13.0.0.15021")]
--- PopForums/Properties/AssemblyInfo.cs
+++ PopForums/Properties/AssemblyInfo.cs
@@ -2,10 +2,6 @@
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("PopForums")]
-[assembly: AssemblyCompany("POP World Media, LLC")]
 [assembly: AssemblyProduct("PopForums")]
-[assembly: AssemblyCopyright("Copyright ©2015, POP World Media, LLC")]
-[assembly: AssemblyTrademark("POP Forums is a trademark of POP World Media, LLC")]
-[assembly: AssemblyVersion("13.0.0.15021")]
 
 [assembly: InternalsVisibleTo("PopForums.Test")]
