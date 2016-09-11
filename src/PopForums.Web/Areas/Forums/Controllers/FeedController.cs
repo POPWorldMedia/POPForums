abc8761315254b511e15657dc05b9925abbfc478
Jeff <jeffyjones@hotmail.com>
2016-09-10 23:54:36 -0400

    More view porting.

--- src/PopForums.Web/Areas/Forums/Controllers/FeedController.cs
+++ src/PopForums.Web/Areas/Forums/Controllers/FeedController.cs
@@ -1,10 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PopForums.Feeds;
 
 namespace PopForums.Web.Areas.Forums.Controllers
 {
 	[Area("Forums")]
 	public class FeedController : Controller
     {
+	    private readonly IFeedService _feedService;
+
 	    public static string Name = "Feed";
-    }
+
+	    public FeedController(IFeedService feedService)
+	    {
+		    _feedService = feedService;
+		}
+
+		public ViewResult Index()
+		{
+			var feed = _feedService.GetFeed();
+			return View(feed);
+		}
+	}
 }
