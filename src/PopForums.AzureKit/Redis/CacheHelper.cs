cfa066ecf222c9247ae77acfe7201d51778614fd
Jeff <jeff@popw.com>
2016-09-24 23:43:48 -0400

    Revert use of Lazy Redis connection... it's functionally equiv to what I had.

--- src/PopForums.AzureKit/Redis/CacheHelper.cs
+++ src/PopForums.AzureKit/Redis/CacheHelper.cs
@@ -9,16 +9,12 @@ namespace PopForums.AzureKit.Redis
     public class CacheHelper : ICacheHelper
     {
 	    private readonly IErrorLog _errorLog;
-	    private static IConfig _config;
+	    private readonly IConfig _config;
 	    private static ConnectionMultiplexer _cacheConnection;
 	    private static ConnectionMultiplexer _messageConnection;
 		private static IMemoryCache _cache;
 		private const string _removeChannel = "pf.cache.remove";
 	    private const string _addChannel = "pf.cache.add";
-
-		private static Lazy<ConnectionMultiplexer> _lazyConnection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(_config.CacheConnectionString));
-
-		private static ConnectionMultiplexer Connection => _lazyConnection.Value;
 
 	    public CacheHelper(IErrorLog errorLog)
 	    {
@@ -26,7 +22,8 @@ namespace PopForums.AzureKit.Redis
 		    _config = new Config();
 		    if (_cacheConnection == null)
 		    {
-			    _cacheConnection = Connection;
+			    _cacheConnection = ConnectionMultiplexer.Connect(_config.CacheConnectionString);
+			    _cacheConnection.PreserveAsyncOrder = false;
 		    }
 			if (_cache == null)
 				SetupLocalCache();
