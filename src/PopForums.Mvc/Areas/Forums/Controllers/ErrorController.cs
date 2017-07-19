1a5f66f2e4669c7fe9502b5bf4019c30024da0ce
Steven Cooney <steven@stevencooney.com>
2017-07-19 16:45:09 +0100

    Add better error handling for live environments

---
+++ src/PopForums.Mvc/Areas/Forums/Controllers/ErrorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PopForums.Mvc.Areas.Forums.Controllers
+{
+	[Area("Forums")]
+	public class ErrorController : Controller
+	{
+		public static string Name = "Error";
+
+		public ActionResult Index(int code)
+		{
+			if (code == 401 || code == 403)
+			{
+				return View("/Areas/Forums/Views/Shared/Forbidden.cshtml");
+			}
+			else if (code == 404)
+			{
+				return View("/Areas/Forums/Views/Shared/NotFound.cshtml");
+			}
+
+			// Throw error below
+			return View("/Areas/Forums/Views/Shared/UnexpectedError.cshtml");
+		}
+
+		public ActionResult ThrowError()
+		{
+			throw new Exception("Manually generated 500 error");
+		}
+	}
+}
