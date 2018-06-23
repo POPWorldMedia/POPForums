de918adaddbed357b6c35bada09ef4e752e28083
Jeff Putz <jeff@popw.com>
2018-06-23 17:13:16 -0400

    Refactor auth middleware into its own class.

---
+++ src/PopForums.Mvc/Areas/Forums/Authorization/PopForumsAuthorizationMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using PopForums.Services;
+
+namespace PopForums.Mvc.Areas.Forums.Authorization
+{
+	public class PopForumsAuthorizationMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public PopForumsAuthorizationMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public Task InvokeAsync(HttpContext context, IUserService userService, IProfileService profileService)
+		{
+			var authResult = context.AuthenticateAsync(PopForumsAuthorizationDefaults.AuthenticationScheme).Result;
+			var identity = authResult?.Principal?.Identity as ClaimsIdentity;
+			if (identity != null)
+			{
+				var user = userService.GetUserByName(identity.Name);
+				if (user != null)
+				{
+					foreach (var role in user.Roles)
+						identity.AddClaim(new Claim(PopForumsAuthorizationDefaults.ForumsClaimType, role));
+					context.Items["PopForumsUser"] = user;
+					var profile = profileService.GetProfile(user);
+					context.Items["PopForumsProfile"] = profile;
+					context.User = new ClaimsPrincipal(identity);
+				}
+			}
+			return _next.Invoke(context);
+		}
+	}
+}
--- src/PopForums.Mvc/Areas/Forums/Extensions/ApplicationBuilders.cs
+++ src/PopForums.Mvc/Areas/Forums/Extensions/ApplicationBuilders.cs
@@ -1,10 +1,6 @@
-﻿using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
 using PopForums.Mvc.Areas.Forums.Authorization;
 using PopForums.Mvc.Areas.Forums.Messaging;
-using PopForums.Services;
 
 namespace PopForums.Mvc.Areas.Forums.Extensions
 {
@@ -24,27 +20,7 @@ namespace PopForums.Mvc.Areas.Forums.Extensions
 
 		public static IApplicationBuilder UsePopForumsAuth(this IApplicationBuilder app)
 		{
-			app.Use(async (context, next) =>
-			{
-				var authResult = context.AuthenticateAsync(PopForumsAuthorizationDefaults.AuthenticationScheme).Result;
-				var identity = authResult?.Principal?.Identity as ClaimsIdentity;
-				if (identity != null)
-				{
-					var userService = app.ApplicationServices.GetService<IUserService>();
-					var user = userService.GetUserByName(identity.Name);
-					if (user != null)
-					{
-						foreach (var role in user.Roles)
-							identity.AddClaim(new Claim(PopForumsAuthorizationDefaults.ForumsClaimType, role));
-						context.Items["PopForumsUser"] = user;
-						var profileService = context.RequestServices.GetService<IProfileService>();
-						var profile = profileService.GetProfile(user);
-						context.Items["PopForumsProfile"] = profile;
-						context.User = new ClaimsPrincipal(identity);
-					}
-				}
-				await next.Invoke();
-			});
+			app.UseMiddleware<PopForumsAuthorizationMiddleware>();
 			return app;
 		}
 	}
