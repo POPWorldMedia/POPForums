a852b7f225ba7b201bc7c6b795b1873e444b2a32
Jeff Putz <jeff@popw.com>
2018-12-09 23:55:27 -0500

    Put assets in an npm package, move signalr out of pf lib

--- src/PopForums.Web/gulpfile.js
+++ src/PopForums.Web/gulpfile.js
@@ -10,7 +10,7 @@ gulp.task("default", function () {
 	var streams = [
 		gulp.src(nodeRoot + "bootstrap/dist/**/*").pipe(gulp.dest(targetPath + "/bootstrap/dist")),
 		gulp.src(nodeRoot + "jquery/dist/**/*").pipe(gulp.dest(targetPath + "/jquery/dist")),
-		gulp.src(nodeRoot + "@aspnet/signalr/dist/**/*").pipe(gulp.dest(targetPath + "/jquery/dist")),
+		gulp.src(nodeRoot + "@aspnet/signalr/dist/browser/**/*").pipe(gulp.dest(targetPath + "/signalr/dist")),
 		gulp.src(nodeRoot + "tinymce/**/*").pipe(gulp.dest(targetPath + "/tinymce"))
 	];
 	return merge(streams);
