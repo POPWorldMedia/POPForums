225390fe934879aae2c29ef0567a2981e1669779
Jeff Putz <jeff@popw.com>
2019-01-19 20:52:21 -0500

    More vue experimentation

---
+++ src/PopForums.Mvc/Areas/Forums/Controllers/AdminApiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PopForums.Models;
+using PopForums.Mvc.Areas.Forums.Authorization;
+using PopForums.Services;
+
+namespace PopForums.Mvc.Areas.Forums.Controllers
+{
+	[Authorize(Policy = PermanentRoles.Admin, AuthenticationSchemes = PopForumsAuthorizationDefaults.AuthenticationScheme)]
+	[Area("Forums")]
+	[Produces("application/json")]
+	public class AdminApiController : ControllerBase
+	{
+		private readonly ICategoryService _categoryService;
+
+		public AdminApiController(ICategoryService categoryService)
+		{
+			_categoryService = categoryService;
+		}
+
+		[HttpGet]
+		public ActionResult<List<Category>> GetCategories()
+		{
+			var categories = _categoryService.GetAll();
+			return categories;
+		}
+	}
+}
--- src/PopForums.Web/gulpfile.js
+++ src/PopForums.Web/gulpfile.js
@@ -12,7 +12,9 @@ gulp.task("default", function () {
 		gulp.src(nodeRoot + "popper.js/dist/umd/popper.min.js").pipe(gulp.dest(targetPath + "/popper.js/dist")),
 		gulp.src(nodeRoot + "jquery/dist/**/*").pipe(gulp.dest(targetPath + "/jquery/dist")),
 		gulp.src(nodeRoot + "@aspnet/signalr/dist/browser/**/*").pipe(gulp.dest(targetPath + "/signalr/dist")),
-		gulp.src(nodeRoot + "tinymce/**/*").pipe(gulp.dest(targetPath + "/tinymce"))
+		gulp.src(nodeRoot + "tinymce/**/*").pipe(gulp.dest(targetPath + "/tinymce")),
+		gulp.src(nodeRoot + "vue/dist/**/*").pipe(gulp.dest(targetPath + "/vue/dist")),
+		gulp.src(nodeRoot + "vue-router/dist/**/*").pipe(gulp.dest(targetPath + "/vue-router/dist"))
 	];
 	return merge(streams);
 });
