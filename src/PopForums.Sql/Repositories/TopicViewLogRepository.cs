609990ec99ce1ad1df8e2efab91dc30144c94581
Jeff Putz <jeff@popw.com>
2019-03-24 23:07:16 -0400

    GITHUB DESKTOP Y U NO COMMIT NEW FILEZ????!!11!?

---
+++ src/PopForums.Sql/Repositories/TopicViewLogRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dapper;
+using PopForums.Repositories;
+
+namespace PopForums.Sql.Repositories
+{
+	public class TopicViewLogRepository : ITopicViewLogRepository
+	{
+		private readonly ISqlObjectFactory _sqlObjectFactory;
+
+		public TopicViewLogRepository(ISqlObjectFactory sqlObjectFactory)
+		{
+			_sqlObjectFactory = sqlObjectFactory;
+		}
+
+		public async Task Log(int? userID, int topicID, DateTime timeStamp)
+		{
+			await _sqlObjectFactory.GetConnection().UsingAsync(connection =>
+				connection.ExecuteAsync("INSERT INTO pf_TopicViewLog (UserID, TopicID, [TimeStamp]) VALUES (@UserID, @TopicID, @TimeStamp)", new {UserID = userID, TopicID = topicID, TimeStamp = timeStamp}));
+		}
+	}
+}
