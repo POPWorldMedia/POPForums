5d7a38e22fe39c132efffb6ad42ae479623648c9
Jeff Putz <jeff@popw.com>
2019-09-07 21:29:02 -0400

    Allow flexibility in dependency ordering.

--- src/PopForums.Mvc/Areas/Forums/Extensions/ServiceCollections.cs
+++ src/PopForums.Mvc/Areas/Forums/Extensions/ServiceCollections.cs
@@ -18,10 +18,24 @@ namespace PopForums.Mvc.Areas.Forums.Extensions
 		/// fails if the ISetupService can't connect to the database or the database isn't set up.
 		/// </summary>
 		/// <param name="services"></param>
-		/// <returns></returns>
+		/// <returns>The updated IServiceCollection.</returns>
 		public static IServiceCollection AddPopForumsMvc(this IServiceCollection services)
 		{
-			services.AddPopForumsBase();
+			return services.AddPopForumsMvc(true);
+		}
+
+		/// <summary>
+		/// Adds web project services to dependency injection container and authentication for POP Forums. This method 
+		/// fails if the ISetupService can't connect to the database or the database isn't set up.
+		/// </summary>
+		/// <param name="services"></param>
+		/// <param name="includePopForumsBaseServices">Indicate false if you intend to call
+		/// services.AddPopForumsBase() on your own.</param>
+		/// <returns>The updated IServiceCollection.</returns>
+		public static IServiceCollection AddPopForumsMvc(this IServiceCollection services, bool includePopForumsBaseServices)
+		{
+			if (includePopForumsBaseServices)
+				services.AddPopForumsBase();
 			services.AddHttpContextAccessor();
 			services.AddPopIdentity();
 			services.AddTransient<IUserRetrievalShim, UserRetrievalShim>();
