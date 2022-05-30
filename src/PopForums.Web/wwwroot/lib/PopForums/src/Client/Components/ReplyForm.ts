ab4a0925e0c3f460f93c108263f5707414adafd3
Jeff Putz <jeff@popw.com>
2022-05-29 20:19:02 -0400

    migrate forms to components #286

---
+++ src/PopForums.Web/wwwroot/lib/PopForums/src/Client/Components/ReplyForm.ts
@@ -0,0 +1,71 @@
+namespace PopForums {
+
+    export class ReplyForm extends HTMLElement {
+        constructor() {
+            super();
+            let template = document.getElementById(this.templateID) as HTMLTemplateElement;
+            if (!template) {
+                console.error(`Can't find templateID ${this.templateID} to make reply form.`);
+                return;
+            }
+            this.append(template.content.cloneNode(true));
+        }
+
+        get templateID() {
+            return this.getAttribute("templateid");
+        }
+
+        private button: HTMLInputElement;
+
+        connectedCallback() {
+            this.button = this.querySelector("#SubmitReply");
+            this.button.addEventListener("click", () => {
+                this.submitReply();
+            });
+        }
+
+        submitReply() {
+            this.button.setAttribute("disabled", "disabled");
+            var closeCheck = document.querySelector("#CloseOnReply") as HTMLInputElement;
+            var closeOnReply = false;
+            if (closeCheck && closeCheck.checked) closeOnReply = true;
+            var model = {
+                Title: (this.querySelector("#NewReply #Title") as HTMLInputElement).value,
+                FullText: (this.querySelector("#NewReply #FullText") as HTMLInputElement).value,
+                IncludeSignature: (this.querySelector("#NewReply #IncludeSignature") as HTMLInputElement).checked,
+                ItemID: (this.querySelector("#NewReply #ItemID") as HTMLInputElement).value,
+                CloseOnReply: closeOnReply,
+                IsPlainText: (this.querySelector("#NewReply #IsPlainText") as HTMLInputElement).value.toLowerCase() === "true",
+                ParentPostID: (this.querySelector("#NewReply #ParentPostID") as HTMLInputElement).value
+            };
+            fetch(PopForums.AreaPath + "/Forum/PostReply", {
+                method: "POST",
+                body: JSON.stringify(model),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            })
+                .then(response => response.json())
+                .then(result => {
+                    switch (result.result) {
+                        case true:
+                            window.location = result.redirect;
+                            break;
+                        default:
+                            var r = this.querySelector("#PostResponseMessage") as HTMLElement;
+                            r.innerHTML = result.message;
+                            this.button.removeAttribute("disabled");
+                            r.style.display = "block";
+                    }
+                })
+                .catch(error => {
+                    var r = this.querySelector("#PostResponseMessage") as HTMLElement;
+                    r.innerHTML = "There was an unknown error while trying to post";
+                    this.button.removeAttribute("disabled");
+                    r.style.display = "block";
+                });
+        };
+    }
+    
+    customElements.define('pf-replyform', ReplyForm);
+}
---
+++ src/PopForums.Web/wwwroot/lib/PopForums/src/Client/Components/TopicForm.ts
@@ -0,0 +1,66 @@
+namespace PopForums {
+
+    export class TopicForm extends HTMLElement {
+        constructor() {
+            super();
+            let template = document.getElementById(this.templateID) as HTMLTemplateElement;
+            if (!template) {
+                console.error(`Can't find templateID ${this.templateID} to make reply form.`);
+                return;
+            }
+            this.append(template.content.cloneNode(true));
+        }
+
+        get templateID() {
+            return this.getAttribute("templateid");
+        }
+
+        private button: HTMLInputElement;
+
+        connectedCallback() {
+            this.button = this.querySelector("#SubmitNewTopic");
+            this.button.addEventListener("click", () => {
+                this.submitTopic();
+            });
+        }
+
+        submitTopic() {
+            this.button.setAttribute("disabled", "disabled");
+            var model = {
+                Title: (this.querySelector("#NewTopic #Title") as HTMLInputElement).value,
+                FullText: (this.querySelector("#NewTopic #FullText")as HTMLInputElement).value,
+                IncludeSignature: (this.querySelector("#NewTopic #IncludeSignature")as HTMLInputElement).checked,
+                ItemID: (this.querySelector("#NewTopic #ItemID")as HTMLInputElement).value,
+                IsPlainText: (this.querySelector("#NewTopic #IsPlainText")as HTMLInputElement).value.toLowerCase() === "true"
+            };
+            fetch(PopForums.AreaPath + "/Forum/PostTopic", {
+                method: "POST",
+                body: JSON.stringify(model),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            })
+                .then(response => response.json())
+                .then(result => {
+                    switch (result.result) {
+                        case true:
+                            window.location = result.redirect;
+                            break;
+                        default:
+                            var r = this.querySelector("#PostResponseMessage") as HTMLElement;
+                            r.innerHTML = result.message;
+                            this.button.removeAttribute("disabled");
+                            r.style.display = "block";
+                    }
+                })
+                .catch(error => {
+                    var r = this.querySelector("#PostResponseMessage") as HTMLElement;
+                    r.innerHTML = "There was an unknown error while trying to post";
+                    this.button.removeAttribute("disabled");
+                    r.style.display = "block";
+                });
+        };
+    }
+    
+    customElements.define('pf-topicform', TopicForm);
+}
