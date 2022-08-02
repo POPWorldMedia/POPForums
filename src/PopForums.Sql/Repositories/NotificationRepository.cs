907092411a947efe23cfd398a8bcc57cd30d1321
Jeff Putz <jeff@popw.com>
2022-08-02 15:13:39 -0400

    cache notification count

--- src/PopForums.Sql/Repositories/NotificationRepository.cs
+++ src/PopForums.Sql/Repositories/NotificationRepository.cs
@@ -5,11 +5,24 @@ namespace PopForums.Sql.Repositories;
 public class NotificationRepository : INotificationRepository
 {
 	private readonly ISqlObjectFactory _sqlObjectFactory;
+	private readonly ICacheHelper _cacheHelper;
 
-	public NotificationRepository(ISqlObjectFactory sqlObjectFactory)
+	public NotificationRepository(ISqlObjectFactory sqlObjectFactory, ICacheHelper cacheHelper)
 	{
 		_sqlObjectFactory = sqlObjectFactory;
+		_cacheHelper = cacheHelper;
 		SqlMapper.AddTypeHandler(new JsonElementTypeHandler());
+	}
+
+	private string GetCacheKey(int userID)
+	{
+		return "PopForums.NewNotifications." + userID;
+	}
+
+	private void RemoveCache(int userID)
+	{
+		var key = GetCacheKey(userID);
+		_cacheHelper.RemoveCacheObject(key);
 	}
 
 	public async Task<int> UpdateNotification(Notification notification)
@@ -17,6 +30,7 @@ public class NotificationRepository : INotificationRepository
 		Task<int> total = null;
 		await _sqlObjectFactory.GetConnection().UsingAsync(connection =>
 			total = connection.ExecuteAsync("UPDATE pf_Notifications SET TimeStamp = @TimeStamp, IsRead = @IsRead, Data = @Data WHERE UserID = @UserID AND NotificationType = @NotificationType AND ContextID = @ContextID", notification));
+		RemoveCache(notification.UserID);
 		return await total;
 	}
 
@@ -24,12 +38,14 @@ public class NotificationRepository : INotificationRepository
 	{
 		await _sqlObjectFactory.GetConnection().UsingAsync(connection =>
 			connection.ExecuteAsync("INSERT INTO pf_Notifications (UserID, NotificationType, ContextID, TimeStamp, IsRead, Data) VALUES (@UserID, @NotificationType, @ContextID, @TimeStamp, @IsRead, @Data)", notification));
+		RemoveCache(notification.UserID);
 	}
 
 	public async Task MarkNotificationRead(int userID, NotificationType notificationType, long contextID)
 	{
 		await _sqlObjectFactory.GetConnection().UsingAsync(connection =>
 			connection.ExecuteAsync("UPDATE pf_Notifications SET IsRead = 1 WHERE UserID = @userID AND NotificationType = @notificationType AND ContextID = @contextID", new { userID, notificationType, contextID }));
+		RemoveCache(userID);
 	}
 
 	public async Task<List<Notification>> GetNotifications(int userID, int startRow, int pageSize)
@@ -70,9 +86,14 @@ SET ROWCOUNT 0";
 
 	public async Task<int> GetUnreadNotificationCount(int userID)
 	{
+		var key = GetCacheKey(userID);
+		var cachedItem = _cacheHelper.GetCacheObject<int?>(key);
+		if (cachedItem != null)
+			return 0;
 		Task<int> count = null;
 		await _sqlObjectFactory.GetConnection().UsingAsync(connection =>
 			count = connection.QuerySingleAsync<int>("SELECT COUNT(*) FROM pf_Notifications WHERE UserID = @userID AND IsRead = 0", new { userID }));
+		_cacheHelper.SetCacheObject(key, count.Result);
 		return count.Result;
 	}
 
@@ -80,6 +101,7 @@ SET ROWCOUNT 0";
 	{
 		await _sqlObjectFactory.GetConnection().UsingAsync(connection =>
 			connection.ExecuteAsync("UPDATE pf_Notifications SET IsRead = 1 WHERE UserID = @userID", new { userID }));
+		RemoveCache(userID);
 	}
 
 	public async Task<DateTime> GetOldestTime(int userID, int takeCount)
