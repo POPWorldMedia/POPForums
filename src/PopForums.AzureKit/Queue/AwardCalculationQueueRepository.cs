95b82e5c3030fd665bc80e79977af9e71f289cd6
Jeff Putz <jeff@popw.com>
2022-08-30 20:14:41 -0400

    catch and log failures on enqueues for Azure, set retry policy #284

--- src/PopForums.AzureKit/Queue/AwardCalculationQueueRepository.cs
+++ src/PopForums.AzureKit/Queue/AwardCalculationQueueRepository.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Azure.Core;
 using Azure.Storage.Queues;
 using PopForums.Configuration;
 using PopForums.Models;
@@ -11,33 +13,44 @@ namespace PopForums.AzureKit.Queue;
 public class AwardCalculationQueueRepository : IAwardCalculationQueueRepository
 {
 	private readonly IConfig _config;
+	private readonly IErrorLog _errorLog;
 	public const string QueueName = "pfawardcalcqueue";
 
-	public AwardCalculationQueueRepository(IConfig config)
+	public AwardCalculationQueueRepository(IConfig config, IErrorLog errorLog)
 	{
 		_config = config;
+		_errorLog = errorLog;
 	}
 
 	public async Task Enqueue(AwardCalculationPayload payload)
 	{
 		var serializedPayload = JsonSerializer.Serialize(payload);
-		var client = await GetQueueClient();
-		await client.SendMessageAsync(serializedPayload);
+		try
+		{
+			var client = await GetQueueClient();
+			await client.SendMessageAsync(serializedPayload);
+		}
+		catch (Exception exc)
+		{
+			_errorLog.Log(exc, ErrorSeverity.Warning, $"AwardCalc enqueue failed on payload: {serializedPayload}");
+		}
 	}
 
 #pragma warning disable 1998
 	public async Task<KeyValuePair<string, int>> Dequeue()
-#pragma warning restore 1998
 	{
-		throw new System.NotImplementedException($"{nameof(Dequeue)} should never be called because it's automatically bound to an Azure function.");
+		throw new NotImplementedException($"{nameof(Dequeue)} should never be called because it's automatically bound to an Azure function.");
 	}
+#pragma warning restore 1998
 
 	private async Task<QueueClient> GetQueueClient()
 	{
-		var client = new QueueClient(_config.QueueConnectionString, QueueName, new QueueClientOptions
+		var options = new QueueClientOptions
 		{
-			MessageEncoding = QueueMessageEncoding.Base64
-		});
+			MessageEncoding = QueueMessageEncoding.Base64,
+			Retry = { NetworkTimeout = TimeSpan.FromSeconds(2), MaxRetries = 1, Mode = RetryMode.Fixed, Delay = TimeSpan.Zero, MaxDelay = TimeSpan.Zero }
+		};
+		var client = new QueueClient(_config.QueueConnectionString, QueueName, options);
 		await client.CreateIfNotExistsAsync();
 		return client;
 	}
--- src/PopForums.AzureKit/Queue/SearchIndexQueueRepository.cs
+++ src/PopForums.AzureKit/Queue/SearchIndexQueueRepository.cs
@@ -1,5 +1,7 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Azure.Core;
 using Azure.Storage.Queues;
 using PopForums.Configuration;
 using PopForums.Models;
@@ -10,33 +12,44 @@ namespace PopForums.AzureKit.Queue;
 public class SearchIndexQueueRepository : ISearchIndexQueueRepository
 {
 	private readonly IConfig _config;
+	private readonly IErrorLog _errorLog;
 	public const string QueueName = "pfsearchindexqueue";
 
-	public SearchIndexQueueRepository(IConfig config)
+	public SearchIndexQueueRepository(IConfig config, IErrorLog errorLog)
 	{
 		_config = config;
+		_errorLog = errorLog;
 	}
 
 	public async Task Enqueue(SearchIndexPayload payload)
 	{
 		var serializedPayload = JsonSerializer.Serialize(payload);
-		var client = await GetQueueClient();
-		await client.SendMessageAsync(serializedPayload);
+		try
+		{
+			var client = await GetQueueClient();
+			await client.SendMessageAsync(serializedPayload);
+		}
+		catch (Exception exc)
+		{
+			_errorLog.Log(exc, ErrorSeverity.Warning, $"SearchIndex enqueue failed on payload: {serializedPayload}");
+		}
 	}
 
 #pragma warning disable 1998
 	public async Task<SearchIndexPayload> Dequeue()
 	{
-		throw new System.NotImplementedException($"{nameof(Dequeue)} should never be called because it's automatically bound to an Azure function.");
+		throw new NotImplementedException($"{nameof(Dequeue)} should never be called because it's automatically bound to an Azure function.");
 	}
 #pragma warning restore 1998
 
 	private async Task<QueueClient> GetQueueClient()
 	{
-		var client = new QueueClient(_config.QueueConnectionString, QueueName, new QueueClientOptions
+		var options = new QueueClientOptions
 		{
-			MessageEncoding = QueueMessageEncoding.Base64
-		});
+			MessageEncoding = QueueMessageEncoding.Base64,
+			Retry = { NetworkTimeout = TimeSpan.FromSeconds(2), MaxRetries = 1, Mode = RetryMode.Fixed, Delay = TimeSpan.Zero, MaxDelay = TimeSpan.Zero }
+		};
+		var client = new QueueClient(_config.QueueConnectionString, QueueName, options);
 		await client.CreateIfNotExistsAsync();
 		return client;
 	}
