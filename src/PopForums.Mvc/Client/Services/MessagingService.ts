e043183f71a61baae800afc22b5be6e876a15a01
Jeff Putz <jeff@popw.com>
2022-09-21 15:05:15 -0400

    refactor messaging service to avoid race conditions

--- src/PopForums.Mvc/Client/Services/MessagingService.ts
+++ src/PopForums.Mvc/Client/Services/MessagingService.ts
@@ -2,21 +2,22 @@
 
 	export class MessagingService {
 		private static service: MessagingService;
+		private static promise: Promise<void>;
 
 		static async GetService(): Promise<MessagingService> {
-			if (!this.service) {
-				let service = new MessagingService();
-				await service.start();
+			if (!this.promise) {
+				const service = new MessagingService();
+				this.promise = service.start();
 				this.service = service;
 			}
+			await Promise.all([this.promise]);
 			return this.service;
 		}
 
 		connection: any;
 
-		async start() {
+		private async start() {
 			this.connection = new signalR.HubConnectionBuilder().withUrl("/PopForumsHub").withAutomaticReconnect().build();
-
 			await this.connection.start();
 		}
 	}
