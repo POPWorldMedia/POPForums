f941d5069fe9bf497358603785465aaf29dbcf5c
Jeff Putz <jeff@popw.com>
2023-02-08 16:43:20 -0500

    Old auth cookie name is not used anymore

--- src/PopForums.Mvc/Areas/Forums/Authorization/PopForumsAuthorizationDefaults.cs
+++ src/PopForums.Mvc/Areas/Forums/Authorization/PopForumsAuthorizationDefaults.cs
@@ -3,7 +3,6 @@
 public static class PopForumsAuthorizationDefaults
 {
 	public const string AuthenticationScheme = "PopForumsAuthScheme";
-	public const string CookieName = "pfauth";
 	public const string ForumsClaimType = "http://popforums.com/forumclaims";
 	public const string ForumsUserIDType = "http://popforums.com/forumuserid";
 }
