2fa284da283e1c3a8f136fdf6e6969fa6b37a45e
Ora Walters <owenwalters.net@gmail.com>
2024-03-22 13:47:45 -0500

    Fix email tests for new regex

--- src/PopForums.Test/Extensions/StringTests.cs
+++ src/PopForums.Test/Extensions/StringTests.cs
@@ -24,16 +24,16 @@ public class StringTests
 		Assert.True("obama@whitehouse.gov".IsEmailAddress());
 		Assert.True("a_b@c.net".IsEmailAddress());
 		Assert.True("a.b@site.co.uk".IsEmailAddress());
+		Assert.True("ora@mixedmedia.studio".IsEmailAddress());
+		Assert.True("mason@日本.com".IsEmailAddress());
 	}
 
 	[Fact]
 	public void IsNoteEmailTest()
 	{
 		Assert.False("a@c".IsEmailAddress());
-		Assert.False("a a@c.com".IsEmailAddress());
-		Assert.False("aa@c a.com".IsEmailAddress());
-		Assert.False("a!a@c.com".IsEmailAddress());
-		Assert.False("aa@coishd!iwe.com".IsEmailAddress());
+		Assert.False("abc@examplecom".IsEmailAddress());
+		Assert.False("ora.mixedmedia.studio".IsEmailAddress());
 	}
 
 	[Fact]
