48724854a0026a77ee31553a4a82d6cd5c36bf57
Jeff Putz <jeff@popw.com>
2024-12-06 13:59:32 -0500

    Update legacy SQL upgrade script

--- src/PopForums.Sql/PopForums16to21.sql
+++ src/PopForums.Sql/PopForums16to21.sql
@@ -170,3 +170,22 @@ IF COL_LENGTH('dbo.pf_QueuedEmailMessage', 'FromEmail') IS NOT NULL
 BEGIN
 	ALTER TABLE pf_QueuedEmailMessage DROP COLUMN FromEmail;
 END
+
+
+
+IF COL_LENGTH('dbo.pf_PopForumsUser', 'TokenExpiration') IS NULL
+    BEGIN
+        ALTER TABLE pf_PopForumsUser ADD [TokenExpiration] [datetime] NULL;
+    END
+
+IF COL_LENGTH('dbo.pf_UserActivity', 'RefreshToken') IS NULL
+    BEGIN
+        ALTER TABLE pf_UserActivity ADD [RefreshToken] [nvarchar](MAX) NULL;
+    END
+
+
+
+IF COL_LENGTH('dbo.pf_Profile', 'Twitter') IS NOT NULL
+    BEGIN
+        ALTER TABLE pf_Profile DROP COLUMN [Twitter];
+    END
